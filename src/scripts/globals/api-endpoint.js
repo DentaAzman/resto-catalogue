d8edfeaeea48584749f94fcd22aadf25cbb6e911
DentaAzman <152490890+DentaAzman@users.noreply.github.com>
2024-05-19 22:10:08 +0700

    Delete src directory

--- src/scripts/globals/api-endpoint.js
+++
@@ -1,8 +0,0 @@
-import CONFIG from './config';
-
-const API_ENDPOINT = {
-  HOME: `${CONFIG.BASE_URL}/list`,
-  DETAIL: (id) => `${CONFIG.BASE_DETAIL_URL}/${id}`,
-};
-
-export default API_ENDPOINT;
